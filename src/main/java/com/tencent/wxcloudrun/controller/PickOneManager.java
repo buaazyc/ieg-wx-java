3d1d89df4d466a4a9ef2ff7dd0115c5f8a560a5e
zhangyichuan <zhangyichuan.zyc@alibaba-inc.com>
2025-07-17 09:15:56 +0800

    feat: 抽盲盒优化

--- src/main/java/com/tencent/wxcloudrun/controller/PickOneManager.java
+++ src/main/java/com/tencent/wxcloudrun/controller/PickOneManager.java
@@ -29,7 +29,7 @@ public class PickOneManager {
 
     public String getOne(WxRequest req, Map<String, IegUserDO> userMap) {
         // 频控逻辑
-        String cacheKey = req.getFromUserName() + "-" + java.time.LocalDate.now();
+        String cacheKey = req.getFromUserName() + "-" + java.time.LocalDate.now() + req.getContent();
         if (cache.getIfPresent(cacheKey) != null) {
             return "您今天已经参与过了，请明天再来！先好好看一下TA的书和问题吧";
         }
