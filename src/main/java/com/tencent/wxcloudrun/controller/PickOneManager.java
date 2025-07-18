61b19996ac4531c5a42e68c64fa902822c2d81b4
zhangyichuan <zhangyichuan.zyc@alibaba-inc.com>
2025-07-18 09:57:53 +0800

    feat: 修改时区

--- src/main/java/com/tencent/wxcloudrun/controller/PickOneManager.java
+++ src/main/java/com/tencent/wxcloudrun/controller/PickOneManager.java
@@ -30,6 +30,7 @@ public class PickOneManager {
     public String getOne(WxRequest req, Map<String, IegUserDO> userMap) {
         // 频控逻辑
         String cacheKey = req.getFromUserName() + "-" + java.time.LocalDate.now() + req.getContent();
+        log.info("cacheKey = {}", cacheKey);
         if (cache.getIfPresent(cacheKey) != null) {
             return "您今天已经参与过了，请明天再来！先好好看一下TA的书和问题吧";
         }
